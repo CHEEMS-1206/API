0a7ef41a5c2ff91dca99c63eb776e5117144babb
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-03-28 20:36:10 +0530

    rendered routes for user in index.js

--- back-end-api-crud/index.js
+++ back-end-api-crud/index.js
@@ -1,12 +1,18 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
+// importing routes for users from usersRoutes
+import userRouter from './routes/usersRoutes.js'
+
 const app = express()
 // defining port for backend rest server
 const PORT = 5000;
 
 // parsing the json
 app.use(bodyParser.json())
+
+// all routes for user are defined here
+app.use('/users',userRouter)
 
 // get route for data to be displayed when browser calls '/'
 app.get('/',(req,res)=>{
