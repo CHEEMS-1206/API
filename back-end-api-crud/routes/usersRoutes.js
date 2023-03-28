f881e41e0a7540052d07eb2f6ccadc20c90b91d8
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-03-28 20:31:36 +0530

    created routes folder for different routes created userRoutes.js for all users routes

---
+++ back-end-api-crud/routes/usersRoutes.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+// dummy database
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 25,
+  },
+];
