3cf60e68a767037c706415033062f51bfdcb5055
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-04-12 15:50:13 +0530

    created a method for adding new users to the database

--- back-end-api-crud/controllers/users.js
+++ back-end-api-crud/controllers/users.js
@@ -1,4 +1,4 @@
-// import userModel from '../models/users';
+import userModel from '../models/users.js';
 
 export const index = (req,res) => {
     res.send('users Index');
@@ -6,4 +6,21 @@ export const index = (req,res) => {
 
 export const listAll = (req, res) => {
   res.send("List of all users");
+};
+
+
+// controller for creating new user in the database
+export const createUser = async (req,res)=> {
+  const newUser = new userModel({
+    name : req.body.name,
+    title : req.body.title,
+    bio : req.body.bio,
+  })
+
+  try{
+    const creatingUser = await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({msg : err})
+  }
 };
