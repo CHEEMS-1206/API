e9c6177dd1bc8c6853cb6e01804a0268ca4efa9b
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-04-13 16:25:32 +0530

    imported uuid and used it to create unique creationId for every new user. also modified _id by changing objectId() to direct string for error resolving

--- back-end-api-crud/models/users.js
+++ back-end-api-crud/models/users.js
@@ -1,23 +1,35 @@
 import mongoose from "mongoose";
+import uuid from "react-uuid";
+const ObjectId = mongoose.Types.ObjectId;
 
 const userSchema = new mongoose.Schema({
-    name : {
-        type : String,
-        required : true,
-        unique : true,
+  _id: {
+    type: String,
+    default: function () {
+      return new ObjectId().toString();
     },
-    title : {
-        type : String,
-        required : true,
-    },
-    bio : {
-        type : String,
-        required : true,
-    },
-    date : {
-        type : Date,
-        default : Date.now,
-    }
-})
-const userModel = mongoose.model('userModel',userSchema);
+  },
+  creationId : {
+    type : String,
+    default : uuid(),
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  bio: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const userModel = mongoose.model("userModel", userSchema);
 export default userModel;
