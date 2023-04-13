c82f470e39563385d5356874b02e1971af4ca62b
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-04-13 16:26:33 +0530

    wrote controllers for home and getting all users and getting a particular user by _id

--- back-end-api-crud/controllers/users.js
+++ back-end-api-crud/controllers/users.js
@@ -1,26 +1,50 @@
-import userModel from '../models/users.js';
+import userModel from "../models/users.js";
 
-export const index = (req,res) => {
-    res.send('users Index');
-}
-
-export const listAll = (req, res) => {
-  res.send("List of all users");
+// /user hit
+export const home = (req, res) => {
+  res.status(200).send("You have reached the endpoint /users !!");
 };
 
-
 // controller for creating new user in the database
-export const createUser = async (req,res)=> {
+export const createUser = async (req, res) => {
   const newUser = new userModel({
-    name : req.body.name,
-    title : req.body.title,
-    bio : req.body.bio,
-  })
+    name: req.body.name,
+    title: req.body.title,
+    bio: req.body.bio,
+  });
 
-  try{
+  try {
     const creatingUser = await newUser.save();
     res.status(201).json(newUser);
   } catch (err) {
-    res.status(400).json({msg : err})
+    res.status(400).json({ msg: err });
+  }
+};
+
+// controller for fetching all users from the database
+export const listAll = async (req, res) => {
+  try {
+    const query = await userModel.find();
+    if (query) {
+      res.status(200).json(query);
+    } else {
+      res.status(400).json({msg : "No user here !"});
+    }
+  } catch (err) {
+    res.status(400).json({ msg: err });
+  }
+};
+
+// controller for fetching a particular user from the database
+export const getUser = async (req, res) => {
+  try {
+    const query = await userModel.findById(req.params.id);
+    if (!query) {
+      res.status(400).json({ msg: "Invalid user request !!" });
+    } else {
+      res.status(200).json(query);
+    }
+  } catch (err) {
+    res.status(400).json({ msg: err });
   }
 };
